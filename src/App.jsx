7c0d817c70443254f3ae113ce7a5fe08a6f03175
Shubham Londhe <shubhamlondhe63@gmail.com>
2024-03-18 15:18:20 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
+    <UserContextProvider>
       <h1>React project to practice Context Api</h1>
-    </>
-  )
+
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
 }
 
-export default App
+export default App;
